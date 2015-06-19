5a3aa440f475f7796b680a219f4149279862921c
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2015-06-19 19:40:48 +0200

    Plotting distance feature against gmm.

--- gmm_spatial_model/scripts/qsr_learn_proto.py
+++ gmm_spatial_model/scripts/qsr_learn_proto.py
@@ -18,6 +18,10 @@ from gmm_spatial_model.spatial_model_service import NearModel
 
 from mongodb_store.message_store import MessageStoreProxy
 from soma_msgs.msg import SOMAObject
+from scipy.spatial.distance import euclidean
+
+
+
 
 def quaternion_msg_to_np(q):
     """
@@ -26,7 +30,7 @@ def quaternion_msg_to_np(q):
     return np.array([q.x, q.y, q.z, q.w])
 
 
-def draw_pose_arrow(ax, pose, arrow_length = 1, annotation = ''):
+def draw_pose_arrow(pose, arrow_length = 1, annotation = ''):
 
     # ax.arrow(landmark_pose.position.x, landmark_pose.position.y, 
     #   , 
@@ -39,7 +43,7 @@ def draw_pose_arrow(ax, pose, arrow_length = 1, annotation = ''):
     dy = arrow_length * sin(angle)
     end = (pose.position.x + dx, pose.position.y + dy)
 
-    ax.annotate(annotation,
+    plt.annotate(annotation,
             xy=end, xycoords='data',
             xytext=start, textcoords='data',
             arrowprops=dict(arrowstyle="->",
@@ -67,11 +71,21 @@ def mkpose(x, y):
     """
     return Pose(Point(x,y,0), Quaternion(*quaternion_from_euler(0, 0, 0)))
 
+def pose_to_xy(pose):
+    return (pose.position.x, pose.position.y)
+
 def pose_list_to_np(pose_list):
     """
     Convert a list of Pose objects into an np array of x,y coords
     """
-    return np.array([[p.position.x, p.position.y] for p in pose_list])
+    return np.array([pose_to_xy(p) for p in pose_list])
+
+
+def distance(landmark, target):
+    return euclidean(pose_to_xy(landmark), pose_to_xy(target))
+
+def to_spatial_relation(landmark, targets, fn):
+    return np.array([[fn(landmark, target)] for target in targets])
 
 if __name__ == '__main__':
 
@@ -88,11 +102,14 @@ if __name__ == '__main__':
     landmark_pose = chair.pose
 
     # some feedback we've got from the user
-    good_samples =  [mkpose(1.8, 1), mkpose(1.9, 2.1), mkpose(1.4, 0.5)]
-    bad_samples = [mkpose(-2, 0.3), mkpose(-0.2, -0.5), mkpose(-2, 0.2)]
+    good_sample_poses =  [mkpose(1.8, 1), mkpose(1.9, 2.1), mkpose(1.4, 0.5)]
+    bad_sample_poses = [mkpose(-2, 0.3), mkpose(-0.2, -0.5), mkpose(-2, 0.2)]
+    all_sample_poses = good_sample_poses + bad_sample_poses
 
-    good_samples = pose_list_to_np(good_samples)
-    bad_samples = pose_list_to_np(bad_samples)
+
+    good_samples = pose_list_to_np(good_sample_poses)
+    bad_samples = pose_list_to_np(bad_sample_poses)
+    
 
     # N = 50
     # x = np.random.rand(N)
@@ -100,12 +117,13 @@ if __name__ == '__main__':
     # colors = np.random.rand(N)
     # area = np.pi * (15 * np.random.rand(N))**2 # 0 to 15 point radiuses
     
-    
+    plt.figure(1)
+    plt.subplot(211)
+# 
     centre_plot_on_pose(landmark_pose, 6)
 
-    ax = plt.axes()
 
-    draw_pose_arrow(ax, landmark_pose, arrow_length = 0.5)
+    draw_pose_arrow(landmark_pose, arrow_length = 0.5)
     obstacles = [cabinet, table]
     plt.scatter([p.pose.position.x for p in obstacles], 
                     [p.pose.position.y for p in obstacles],
@@ -114,29 +132,53 @@ if __name__ == '__main__':
 
     plt.scatter(good_samples[:,0], good_samples[:,1], c='g', s = 40)
     plt.scatter(bad_samples[:,0], bad_samples[:,1], c='r', s = 40)
+    
+
+    distances = to_spatial_relation(cabinet.pose, all_sample_poses, distance)
+    print distances
+
+    ax1 = plt.subplot(212)
+    n, bins, patches = ax1.hist(distances, 20, normed=1, facecolor='green', alpha=0.5)
+    
+
+    ax2 = ax1.twinx()
 
     clf = mixture.GMM(n_components=2, covariance_type='full')
-    # clf = mixture.DPGMM(n_components=2, covariance_type='full')
+    
 
-    clf.fit(np.concatenate((good_samples,bad_samples)))
+    clf.fit(distances)
 
-    for mean, covar in zip(clf.means_, clf._get_covars()):
-        v, w = linalg.eigh(covar)
-        u = w[0] / linalg.norm(w[0])
+    # for mean, covar in zip(clf.means_, clf._get_covars()):
+    x_lim = plt.xticks()
+    
+    x_range = [[x] for x in np.arange(x_lim[0][0], x_lim[0][-1], 0.01)]
+    
+    probs, resps = clf.score_samples(x_range)
+    ax2.plot(x_range, probs)
 
-        # as the DP will not use every component it has access to
-        # unless it needs it, we shouldn't plot the redundant
-        # components.
-        # if not np.any(Y_ == i):
-            # continue
 
-        # Plot an ellipse to show the Gaussian component
-        angle = np.arctan(u[1] / u[0])
-        angle = 180 * angle / np.pi  # convert to degrees
-        ell = mpl.patches.Ellipse(mean, v[0], v[1], 180 + angle, color='yellow')
-        ell.set_clip_box(ax.bbox)
-        ell.set_alpha(0.5)
-        ax.add_artist(ell)
+    # clf = mixture.GMM(n_components=2, covariance_type='full')
+    # # clf = mixture.DPGMM(n_components=2, covariance_type='full')
+
+    # clf.fit(np.concatenate((good_samples,bad_samples)))
+
+    # for mean, covar in zip(clf.means_, clf._get_covars()):
+    #     v, w = linalg.eigh(covar)
+    #     u = w[0] / linalg.norm(w[0])
+
+    #     # as the DP will not use every component it has access to
+    #     # unless it needs it, we shouldn't plot the redundant
+    #     # components.
+    #     # if not np.any(Y_ == i):
+    #         # continue
+
+    #     # Plot an ellipse to show the Gaussian component
+    #     angle = np.arctan(u[1] / u[0])
+    #     angle = 180 * angle / np.pi  # convert to degrees
+    #     ell = mpl.patches.Ellipse(mean, v[0], v[1], 180 + angle, color='yellow')
+    #     ell.set_clip_box(ax.bbox)
+    #     ell.set_alpha(0.5)
+    #     ax.add_artist(ell)
 
     # create a near model for testing
     # a good way of sanity checking things
