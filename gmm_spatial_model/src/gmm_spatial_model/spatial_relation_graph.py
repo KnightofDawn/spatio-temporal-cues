47d21d238f9e93f18c748ef7ca7ed0d5f67aab72
g-gemignani <guglielmogemignani@gmail.com>
2015-06-24 15:23:02 +0200

    added spatial relation graph class

---
+++ gmm_spatial_model/src/gmm_spatial_model/spatial_relation_graph.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+class SpatialRelationGraph(object):
+	"""docstring for SpatialRelationGraph"""
+	def __init__(self, graph = None):
+		super(SpatialRelationGraph, self).__init__()
+		self.graph = graph
+		
+	def add_relation_to_graph(relation):
+		self.graph.append(relation)
